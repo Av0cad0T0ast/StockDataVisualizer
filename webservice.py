f24dcdb78209a8ed332f09cefdbc103c1477c647
Ryan Esparza <rte0503@yahoo.com>
2024-10-21 13:39:34 -0500

    Added comments for functions to be created and added the API key

--- webservice.py
+++ webservice.py
@@ -1,4 +1,53 @@
 import requests
 import pygal
 from datetime import datetime
-import json
+
+API_KEY = '5JUJV1WUJIBMM95'
+
+# Function to get stock data
+
+
+# Function to validate date
+
+
+# Function to plot chart
+
+
+    # Create chart type
+
+
+    # Extract data
+
+
+    # Set chart title and values
+
+
+    # Render chart to an svg and open in browser
+
+
+# Main Loop
+
+
+    # Ask user to select chart type
+
+
+    # Ask user to select time series function
+
+
+    # Map users choice to API function names
+
+
+    # Fetch stock data from Alpha Vantage
+
+
+    # Filter data based on the date range
+
+
+    # Plot chart based on user's choice
+
+
+    # Ask if user wants to continue
+
+
+# if __name__ == "__main__":
+    # main()
